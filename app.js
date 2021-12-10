9a428fd56215571f02c27945e4797bcdf1ed8bba
Nickolaj <nickolaj99@outlook.com>
2021-12-10 07:22:27 +0100

    fixed various bugs and issues

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.use('/getimg', imgRouter);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://jovial-clarke-a9896e.netlify.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
